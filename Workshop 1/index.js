035ffc10ad80c029d61d2a69251db1cfd361938c
Katerina Trifunovska <trifunovskak@gmail.com>
2024-05-18 17:25:34 +0200

    Exercise 5

---
+++ Workshop 1/index.js
@@ -0,0 +1,11 @@
+// Check Age for Voting Eligibility
+
+let age = 17;
+
+console.log(age >= 18 ? "The Person can vote" : "The Person can't vote")
+
+if (age >= 18){
+    console.log("The Person can vote")
+} else {
+    console.log("The Person can't vote")
+}
