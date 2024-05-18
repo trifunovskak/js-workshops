2c3bd596388f3c2fd0597f2dd88550d2b85044d6
Katerina Trifunovska <trifunovskak@gmail.com>
2024-05-18 17:06:03 +0200

    Exercise 2

---
+++ Workshop 1/index.js
@@ -0,0 +1,17 @@
+// 2. Checking Day of the Week
+// - Monday - Start of the work week!
+// - Wednesday - Midweek!
+// - Friday - Almost weekend!
+// - Other day - Just another day.
+
+let day = "Monday";
+
+if (day === "Monday"){
+    console.log("Start of the work week!")
+} else if (day === "Wednesday"){
+    console.log("Midweek!")
+} else if (day === "Friday"){
+    console.log("Almost weekend!")
+} else {
+    console.log("Just another day.")
+}
