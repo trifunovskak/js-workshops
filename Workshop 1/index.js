e349c9f5b32752cd4086124c5aff266398fb50f7
Katerina Trifunovska <trifunovskak@gmail.com>
2024-05-18 17:10:34 +0200

    Exercise 3

---
+++ Workshop 1/index.js
@@ -0,0 +1,18 @@
+// 3. Determining Weather Advice
+// - sunny - Wear sunglasses!
+// - rainy - Take an umbrella!
+// - snowy - Wear a warm coat!
+// - any other - Check the weather forecast for more details.
+
+
+let weather = "sunny";
+
+if (weather === "sunny"){
+    console.log("Wear sunglasses!")
+} else if (weather === "rainy"){
+    console.log("Take an umbrella!")
+} else if (weather === "snowy"){
+    console.log("Wear a warm coat!")
+} else {
+    console.log("Check the weather forecast for more details.")
+}
