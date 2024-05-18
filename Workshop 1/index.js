b385c9e165d76dfa533c0ef4b8fd8b647946ced6
Katerina Trifunovska <trifunovskak@gmail.com>
2024-05-18 17:29:34 +0200

    Exercise 6

---
+++ Workshop 1/index.js
@@ -0,0 +1,11 @@
+// Determining Pass or Fail Based on Score 0-100 ( below 65 is a fail )
+
+let score = 65;
+
+console.log(score >= 65 ? "Pass" : "Fail")
+
+if (score >= 65){
+    console.log("Pass")
+} else {
+    console.log("Fail")
+}
