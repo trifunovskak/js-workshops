69d5bd6475c58e322cdead512ccd6531592658d8
Katerina Trifunovska <trifunovskak@gmail.com>
2024-05-18 17:35:46 +0200

    Exercise 7

---
+++ Workshop 1/index.js
@@ -0,0 +1,11 @@
+// Check temperature for comfort level ( comfort is between 20 and 25 )
+
+let temperature = 20;
+
+console.log(temperature > 20 && temperature < 25 ? "comfortable weather" : "uncomfortable weather")
+
+if (temperature > 20 && temperature < 25){
+    console.log("comfortable weather")
+} else {
+    console.log("uncomfortable weather")
+}
